36799f30a2f02475e76680f3140c11058f6116ac
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-11-22 13:14:16 -0600

    Add documentation for Name and Pair

--- src/main/java/util/Name.java
+++ src/main/java/util/Name.java
@@ -1,26 +1,79 @@
 package util;
 
+/**
+ * The {@code Name} class encapsulates title, first, middle, and last names into
+ * a single object. These names may be formatted in {@link #firstLast()
+ * "first last"},  {@link #lastFirst() "last first"}, and
+ * {@link #lastCommaFirst() "last, first"} format. Additionally, they can be
+ * retrieved individually.
+ *
+ * @author Oliver Abdulrahim
+ */
 public final class Name {
 
     public static final Name NULL_NAME =
             new Name("Pres.", "Barack", "Hussein", "Obama");
 
+    /**
+     * Stores the title of this {@code Name}, for example, {@code "Mrs."} or
+     * {@code "Dr."}.
+     */
     private final String title;
 
+    /**
+     * Stores the first name of this {@code Name}, for example,
+     * {@code "Daniel"}.
+     *
+     * <p> This value must be specified during construction.
+     */
     private final String first;
 
+    /**
+     * Stores the middle name of this {@code Name}, for example,
+     * {@code "Green"}.
+     */
     private final String middle;
 
+    /**
+     * Stores the last name of this {@code Name}, for example, {@code "Smith"}.
+     *
+     * <p> This value must be specified during construction.
+     */
     private final String last;
 
+    /**
+     * Constructs a {@code Name} with the first and last given arguments.
+     *
+     * @param first The first name of the object.
+     * @param last THe last name of the object.
+     */
     public Name(String first, String last) {
         this("", first, "", last);
     }
 
+    /**
+     * Constructs a {@code Name} with the first, middle, and last given
+     * arguments.
+     *
+     * @param first The first name of the object.
+     * @param middle The middle name of the object.
+     * @param last The last name of the object.
+     */
     public Name(String first, String middle, String last) {
         this("", first, middle, last);
     }
 
+    /**
+     * Constructs a {@code Name} with the first, middle, and last given
+     * arguments.
+     *
+     * <p> This constructor normalizes the capitalization of the given names.
+     *
+     * @param title The title of the object.
+     * @param first The first name of the object.
+     * @param middle The middle name of the object.
+     * @param last The last name of the object.
+     */
     public Name(String title, String first, String middle, String last) {
         this.title = title;
         this.first = first;
@@ -28,38 +81,93 @@ public final class Name {
         this.last = last;
     }
 
+    /**
+     * Returns the title of this {@code Name}, or an empty {@code String} if
+     * none (or {@code null}) was provided.
+     *
+     * @return The title of this object.
+     */
     public String title() {
         return title;
     }
 
+    /**
+     * Returns the first name of this {@code Name}, or an empty {@code String}
+     * if none (or {@code null}) was provided.
+     *
+     * @return The first name of this object.
+     */
     public String first() {
         return first;
     }
 
+    /**
+     * Returns the middle name of this {@code Name}, or an empty {@code String}
+     * if none (or {@code null}) was provided.
+     *
+     * @return The middle name of this object.
+     */
     public String middle() {
         return middle;
     }
 
+    /**
+     * Returns the last name of this {@code Name}, or an empty {@code String} if
+     * none (or {@code null}) was provided.
+     *
+     * @return The last name of this object.
+     */
     public String last() {
         return last;
     }
 
+    /**
+     * Returns a {@code String} containing all names stored within this object.
+     * This includes the title, first, middle, and last names, in that order.
+     *
+     * @return The full name represented by this object.
+     */
     public String fullName() {
         return title() + ' ' + first() + ' ' + middle()  + ' ' + last();
     }
 
+    /**
+     * Returns a {@code String} containing the first name followed by the last
+     * name, delimited by a space character.
+     *
+     * @return The first name followed by the last name.
+     */
     public String firstLast() {
         return first() + ' ' + last();
     }
 
+    /**
+     * Returns a {@code String} containing the last name followed by the first
+     * name, delimited by a space character.
+     *
+     * @return The first name followed by the last name.
+     */
     public String lastFirst() {
         return last() + ' ' + first();
     }
 
+    /**
+     * Returns a {@code String} containing the last name followed by the first
+     * name, delimited by a comma and a space.
+     *
+     * @return The first name followed by the last name.
+     */
     public String lastCommaFirst() {
         return last() + ',' + ' ' + first();
     }
 
+    /**
+     * Returns a {@code String} representation of this name, containing the
+     * title, first, middle, and last names, in that order.
+     *
+     * @return A {@code String} representation of this name.
+     * @see #firstLast()
+     */
     @Override
     public String toString() {
         return firstLast();
--- src/main/java/util/Pair.java
+++ src/main/java/util/Pair.java
@@ -2,33 +2,74 @@ package util;
 
 import java.io.Serializable;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.function.Function;
 
+/**
+ * A {@link Comparable Comparable}, mutable implementation of {@link Map.Entry
+ * Entry} that stores a non-nullable pair of two objects.
+ *
+ * @param <A> The type of the key.
+ * @param <B> The type of the value.
+ * @author Oliver Abdulrahim
+ */
 public final class Pair<A, B>
     implements Entry<A, B>, Comparable<Pair<A, B>>, Serializable
 {
 
+    /**
+     * The key for this {@code Pair}, not {@code null}.
+     */
     private A key;
 
+    /**
+     * The value for this {@code Pair}, not {@code null}.
+     */
     private B value;
 
+    /**
+     * Creates a {@code Pair} with the given key-value mapping.
+     *
+     * @param key The key for the {@code Pair}.
+     * @param value The value for the {@code Pair}.
+     * @throws NullPointerException if the given key is {@code null} or if the
+     *         given {@code value} is {@code null}.
+     */
     public Pair(A key, B value) {
         this.key = Objects.requireNonNull(key);
         this.value = Objects.requireNonNull(value);
     }
 
+    /**
+     * Returns the key corresponding with this {@code Pair}, not {@code null}.
+     *
+     * @return The key corresponding with this {@code Pair}.
+     */
     @Override
     public A getKey() {
         return key;
     }
 
+    /**
+     * Returns the value corresponding to this {@code Pair}, not {@code null}.
+     *
+     * @return The value corresponding to this {@code Pair}.
+     */
     @Override
     public B getValue() {
         return value;
     }
 
+    /**
+     * Replaces the key corresponding to this {@code Pair} with the given one,
+     * returning the key previously stored.
+     *
+     * @param key The new key to be stored in this {@code Pair}.
+     * @return The old key replaced by the given one.
+     * @throws NullPointerException if the given key is {@code null}.
+     */
     public A setKey(A key) {
         Objects.requireNonNull(key);
         A replaced = this.key;
@@ -36,6 +77,14 @@ public final class Pair<A, B>
         return replaced;
     }
 
+    /**
+     * Replaces the value corresponding to this {@code Pair} with the given one,
+     * returning the value previously stored.
+     *
+     * @param value The new value to be stored in this {@code Pair}.
+     * @return The old value replaced by the given one.
+     * @throws NullPointerException if the given value is {@code null}.
+     */
     @Override
     public B setValue(B value) {
         Objects.requireNonNull(value);
@@ -44,15 +93,42 @@ public final class Pair<A, B>
         return replaced;
     }
 
+    /**
+     * Applies the given {@code Function} to this object, returning the result.
+     *
+     * @param mapper The mapping function.
+     * @param <R> The type of the result.
+     * @return The value produced by applying the given {@code Function} to this
+     *         object.
+     */
     public <R> R map(Function<Pair<? super A, ? super B>, ? extends R> mapper) {
         return mapper.apply(this);
     }
 
+    /**
+     * Compares the given {@code Pair} to this one for order, returning a
+     * negative number, zero, or a positive number if this object is less than,
+     * equal to, or greater than the given object, respectively.
+     *
+     * @param other The {@code Pair} to compare to this one.
+     * @return A negative number, zero, or a positive number if this object is
+     *         less than, equal to, or greater than the given object,
+     *         respectively.
+     */
     @Override
     public int compareTo(Pair<A, B> other) {
         return Objects.compare(this, other, Comparator.naturalOrder());
     }
 
+    /**
+     * Tests the given object against this one for equality, returning
+     * {@code true} if and only if the given object has the same key-value
+     * mapping as this one. {@code false} otherwise.
+     *
+     * @param other The object to test against this one.
+     * @return {@code true} if the given object is identical to this one,
+     *         {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) {
@@ -66,6 +142,12 @@ public final class Pair<A, B>
             && Objects.equals(this.value, pair.value);
     }
 
+    /**
+     * Computes and returns a hash code value for the key-value mapping in this
+     * object.
+     *
+     * @return A hash code value for this object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(key, value);
