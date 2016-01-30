5ca83e9f9349c27be489153a2f7843c89a5c85f0
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2016-01-30 13:03:16 -0600

    Add documentation for Table

--- src/main/java/sql/Table.java
+++ src/main/java/sql/Table.java
@@ -1,27 +1,61 @@
 package sql;
 
+/**
+ * A class representation of an SQL table.
+ *
+ * @param <T> The type of Java object the table represents.
+ * @author Oliver Abdulrahim
+ */
 public final class Table<T> {
 
-    private final String tableName;
+    /**
+     * The name of the table in SQL.
+     */
+    private final String name;
 
+    /**
+     * The {@code Class} of the Java object this {@code Table} represents.
+     */
     private final Class<T> targetClass;
 
-    public Table(String tableName, Class<T> targetClass) {
-        this.tableName = tableName;
+    /**
+     * Constructs a table with the given arguments.
+     *
+     * @param name The name of the {@code Table}.
+     * @param targetClass The {@code Class} the {@code Table} represents.
+     */
+    public Table(String name, Class<T> targetClass) {
+        this.name = name;
         this.targetClass = targetClass;
     }
 
-    public String getTableName() {
-        return tableName;
+    /**
+     * Returns the name of the table.
+     *
+     * @return The name of the table.
+     */
+    public String getName() {
+        return name;
     }
 
+    /**
+     * Returns the {@code Class} of the Java object this {@code Table}
+     * represents.
+     *
+     * @return the {@code Class} this {@code Table} represents.
+     */
     public Class<T> getTargetClass() {
         return targetClass;
     }
 
+    /**
+     * Returns a {@code String} containing the name of this {@code Table}.
+     *
+     * @return A {@code String} containing the name of this {@code Table}.
+     */
     @Override
     public String toString() {
-        return tableName;
+        return name;
     }
 
 }
